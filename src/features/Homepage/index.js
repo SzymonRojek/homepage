08301fc91688379bef450fef2da7bd8699222253
Szymon Rojek <sz.rojek@gmail.com>
2021-07-15 20:28:59 +0100

    add theme switcher to the WrapperHomepage

--- src/features/Homepage/index.js
+++ src/features/Homepage/index.js
@@ -1,8 +1,10 @@
 import { WrapperHomepage } from "./styled";
 import { Header } from "./Header";
+import { ThemeSwitch } from "../../common/ThemeSwitch";
 
 export const Homepage = () => (
   <WrapperHomepage>
+    <ThemeSwitch />
     <Header />;
   </WrapperHomepage>
 );
