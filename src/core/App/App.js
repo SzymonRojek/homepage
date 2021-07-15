88ef806a1a09c358a91fb2e5bf2afa67b51f99fe
Szymon Rojek <sz.rojek@gmail.com>
2021-07-15 20:27:52 +0100

    create core dir and transfer files

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
-import { GlobalStyle } from "../GlobalStyle";
-import { Container } from "../styled";
-import theme from "../theme";
-import { Homepage } from "../features/Homepage";
+import { GlobalStyle } from "./GlobalStyle";
+import { Container } from "../../styled";
+import { themeLight, themeDark } from "./theme";
+import { Homepage } from "../../features/Homepage";
 
 export const App = () => (
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={themeLight}>
     <Normalize />
     <GlobalStyle />
     <Container>
