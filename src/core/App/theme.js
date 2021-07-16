43b29d83a6cc8f99a28399dd0f4b5dceae5d741a
Szymon Rojek <sz.rojek@gmail.com>
2021-07-16 13:37:47 +0100

    fix bug in the key ironTransparent colorNames

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -5,7 +5,7 @@ const colorNames = {
   shipCove: "#6D93BE",
   anakiwa: "#8CC2FF",
   tropicalBlue: "#CDE0F7",
-  ironTransparent: "rgba(209, 213, 218, 0.3",
+  ironTransparent: "rgba(209, 213, 218, 0.3)",
   white: "#FFFFFF",
   whiteLilac: "#FBFBFE",
   porcelain: "#F2F3F4",
