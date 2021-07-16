0d8fbb321f1beb21628e88040c6f94fbca983a21
Szymon Rojek <sz.rojek@gmail.com>
2021-07-16 21:41:24 +0100

    add one more reducer and also middleware

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import homepageReducer from "../features/Homepage/homepageSlice";
 import themeReducer from "../common/themeSlice";
+import saga from "./saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
+    homepage: homepageReducer,
     theme: themeReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(saga);
 
 export default store;
