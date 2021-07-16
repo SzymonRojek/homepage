bf0a5288f079d71971e35c7d7cadeb0fc8796f0b
Szymon Rojek <sz.rojek@gmail.com>
2021-07-16 15:14:10 +0100

    add Portfolio to the Homepage render function

--- src/features/Homepage/index.js
+++ src/features/Homepage/index.js
@@ -1,12 +1,14 @@
 import { WrapperHomepage } from "./styled";
 import { ThemeSwitch } from "../../common/ThemeSwitch";
 import { MainHeader } from "./MainHeader";
+import { Portfolio } from "./Portfolio";
 import { Footer } from "./Footer";
 
 export const Homepage = () => (
   <WrapperHomepage>
     <ThemeSwitch />
     <MainHeader />
+    <Portfolio />
     <Footer />
   </WrapperHomepage>
 );
