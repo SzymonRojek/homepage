dc4a3e105f08c773203b2c6a279993ad2d506e55
Szymon Rojek <sz.rojek@gmail.com>
2021-07-16 21:36:19 +0100

    fetch repositories, use effect

---
+++ src/features/Homepage/Components/Portfolio/index.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Content } from "./Content";
+import { Section, Header, StyledGithubIcon, MyRecentProjects } from "./styled";
+// import { SubHeader } from "../SubHeader";
+import { githubUserName } from "./githubUserName";
+import {
+  fetchRepositories,
+  selectRepositories,
+  selectRepositoriesStatus,
+} from "../../homepageSlice";
+
+export const Portfolio = () => {
+  const dispatch = useDispatch();
+
+  const repositoriesStatus = useSelector(selectRepositoriesStatus);
+  const repositories = useSelector(selectRepositories);
+
+  useEffect(() => {
+    dispatch(fetchRepositories(githubUserName));
+  }, [dispatch]);
+
+  return (
+    <Section>
+      <Header>
+        <StyledGithubIcon />
+        {/* <SubHeader>Portflio</SubHeader> */}
+        <h2>portfolio</h2>
+        <MyRecentProjects>My recent projects</MyRecentProjects>
+      </Header>
+
+      <Content status={repositoriesStatus} repositories={repositories} />
+    </Section>
+  );
+};
