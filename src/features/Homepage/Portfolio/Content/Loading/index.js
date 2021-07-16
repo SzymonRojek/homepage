30e5e542745b32f87d59871b0fbcf8827d14b502
Szymon Rojek <sz.rojek@gmail.com>
2021-07-16 21:27:05 +0100

    transfer files into different dir

--- src/features/Homepage/Portfolio/Content/Loading/index.js
+++
@@ -1,8 +0,0 @@
-import { StyledSpinner, Wrapper } from "./styled";
-
-export const Loading = () => (
-  <Wrapper>
-    Please wait, projects are&nbsp;being&nbsp;loaded...
-    <StyledSpinner />
-  </Wrapper>
-);
