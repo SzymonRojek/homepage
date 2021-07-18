150ffee64f19d526549cf344577cb8d49c75bd7b
Szymon Rojek <sz.rojek@gmail.com>
2021-07-18 18:12:40 +0100

    remove return and console.log

--- src/features/Homepage/Components/Portfolio/Content/Repositories/index.js
+++ src/features/Homepage/Components/Portfolio/Content/Repositories/index.js
@@ -1,3 +1,4 @@
+import { createGlobalStyle } from "styled-components";
 import {
   Description,
   Tile,
@@ -9,26 +10,23 @@ import {
   LinksValue,
 } from "./styled";
 
-export const Repositories = ({ repositories }) => {
-  console.log(repositories);
-  return (
-    <List>
-      {repositories.map(({ id, name, description, homepage, html_url }) => (
-        <Tile key={id}>
-          <Name>{name}</Name>
-          <Description>{description}</Description>
-          <Links>
-            <LinksRow>
-              <dt>Code:</dt>
-              <LinksValue>
-                <Link target="_blank" rel="noreferrer" href={html_url}>
-                  GitHub Repository
-                </Link>
-              </LinksValue>
-            </LinksRow>
-          </Links>
-        </Tile>
-      ))}
-    </List>
-  );
-};
+export const Repositories = ({ repositories }) => (
+  <List>
+    {repositories.map(({ id, name, description, html_url }) => (
+      <Tile key={id}>
+        <Name>{name}</Name>
+        <Description>{description}</Description>
+        <Links>
+          <LinksRow>
+            <dt>Code:</dt>
+            <LinksValue>
+              <Link target="_blank" rel="noreferrer" href={html_url}>
+                GitHub Repository
+              </Link>
+            </LinksValue>
+          </LinksRow>
+        </Links>
+      </Tile>
+    ))}
+  </List>
+);
