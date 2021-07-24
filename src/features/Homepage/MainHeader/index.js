b29a8a0b2f4730f1bd763e2ae29180517b9a867d
Szymon Rojek <sz.rojek@gmail.com>
2021-07-24 23:18:29 +0100

    remove return keyword from the main header

--- src/features/Homepage/MainHeader/index.js
+++ src/features/Homepage/MainHeader/index.js
@@ -13,23 +13,21 @@ import {
   ButtonIcon,
 } from "./styled";
 
-export const MainHeader = () => {
-  return (
-    <Wrapper>
-      <Avatar src={szymonRojekProfile} alt="Szymon Rojek" />
-      <Details>
-        <ThisIs>this is</ThisIs>
-        <Name>Szymon Rojek</Name>
-        <Summary>
-          <PersonIcon src={personIcon} alt="" />
-          I'm passionate about JavaScript and React, ready for my Junior Front
-          End Developer role.
-        </Summary>
-        <StyledButtonLink href={`mailto:${email}`} title={email}>
-          <ButtonIcon />
-          Hire Me
-        </StyledButtonLink>
-      </Details>
-    </Wrapper>
-  );
-};
+export const MainHeader = () => (
+  <Wrapper>
+    <Avatar src={szymonRojekProfile} alt="Szymon Rojek" />
+    <Details>
+      <ThisIs>this is</ThisIs>
+      <Name>Szymon Rojek</Name>
+      <Summary>
+        <PersonIcon src={personIcon} alt="" />
+        I'm passionate about JavaScript and React, ready for my Junior Front End
+        Developer role.
+      </Summary>
+      <StyledButtonLink href={`mailto:${email}`} title={email}>
+        <ButtonIcon />
+        Hire Me
+      </StyledButtonLink>
+    </Details>
+  </Wrapper>
+);
